8e7824b2168238b378b7670c4c08b1f82a024b9d
Julio Cesar <jccintr@gmail.com>
2026-07-31 21:06:13 -0300

    aplicado estilos na tela Preload

--- src/screens/Preload.jsx
+++ src/screens/Preload.jsx
@@ -1,6 +1,7 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View, ActivityIndicator } from 'react-native'
 import { useEffect } from 'react';
 import { useAuth } from '../contexts/AuthContext'
+import {colors, fonts, fontSizes, radius, spacing} from '../theme/theme';
 
 const Preload = ({navigation}) => {
   const { isAuthenticated } = useAuth();
@@ -16,13 +17,36 @@ const Preload = ({navigation}) => {
 
 
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Preload</Text>
-      <Button title='ir para Login' onPress={() => navigation.navigate('login')}/>
+    <View style={styles.container}>
+      <Text style={styles.h1}>Delivroo Express</Text>
+      <ActivityIndicator style={{marginTop: 40}} size="large" color={colors.cream} />
     </View>
   )
 }
 
 export default Preload
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+      flexGrow: 1,
+      padding: spacing.xxl,
+      justifyContent: 'center',
+      backgroundColor: colors.orange
+    },
+    illustration: {
+      width: '100%',
+      height: 150,
+      borderRadius: radius.xl,
+      backgroundColor: colors.orangeDark,
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginBottom: spacing.xl,
+    },
+    h1: {
+      fontFamily: 'Baloo2_700Bold',
+      color: colors.cream,
+      fontSize: 34,
+      marginBottom: spacing.xxl,
+      textAlign: 'center',
+    },
+})
